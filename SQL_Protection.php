db05e5d6b245a5441a7affd354b1543732e87944
Gorrian <95492658+Gorrian@users.noreply.github.com>
2022-02-16 17:53:35 +0100

    Functions upload
    The file containing the 2 functions have been uploaded

---
+++ SQL_Protection.php
@@ -0,0 +1,34 @@
+<?php
+function StringSanitize (string $Value){
+    $Forbidden=array('#', '=', '\'', ';','\\','+','-');
+    $Result="";
+    $Char=str_split($Value);
+    for($i=0;$i<count($Char);$i++){
+        $Change=false;
+        for($i2=0;$i2<count($Forbidden) && !$Change;$i2++){
+            if($Char[$i]==$Forbidden[$i2]){
+                $Change=true;
+            }
+        }
+        
+        if($Change){
+            $Result = $Result."\\".$Char[$i];
+        }else{
+            $Result = $Result.$Char[$i];
+        }
+        
+        
+    }
+    return $Result;
+}
+function SQLProtection (array $Form){
+    foreach($Form as $Index => $Value){
+        if(is_array($Value)){
+            $Form[$Index]=SQLProtection($Value);
+        }else{
+            $Form[$Index]=StringSanitize($Value);
+        }
+    }
+    return $Form;
+}
+?>
